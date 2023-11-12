d61eeb7d803c8541c870cc5106cdee26b7491e81
TUXINAGRUI <tuxr666@163.com>
2023-11-12 21:40:49 +0800

    test: 触发打包

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'dumi';
-const repo = 'da-li-mao-ui'; 
+const repo = 'da-li-mao-ui';
 export default defineConfig({
   title: 'da-li-mao-ui',
   favicon:
@@ -7,15 +7,18 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-//   base:'/da-li-mao-ui/',
-//   publicPath:'/da-li-mao-ui/',
-//   exportStatic:{}
+  //   base:'/da-li-mao-ui/',
+  //   publicPath:'/da-li-mao-ui/',
+  //   exportStatic:{},
+
   devServer: {
-    port: 1998 // 自定义端口号
+    port: 2023,
   },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/'
-
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  themeConfig: {
+    nav: [{ title: '标题', link: '链接' }],
+  },
 
   // more config: https://d.umijs.org/config
 });
